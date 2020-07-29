71134716ac29340482d2ad4609049ba97340ca79
Kyle Wilson <kyle.logan.wilson@gmail.com>
2020-07-28 17:29:32 -0700

    making revisions for jappl submission

--- closure_risk_sim_meta2_plot.R
+++ closure_risk_sim_meta2_plot.R
@@ -1,6 +1,6 @@
 ################################################
 
-# Closure risk simulation
+# Closure risk simulation & plot Wild + Enhanced separately
 # Nov. 28, 2019 - adding bootstrap for metapopulation within each period
 
 ################################################
@@ -9,14 +9,14 @@ library(Rmisc) #calculate confidence intervals
 library(unikn) 
  
 #mean abundances for each period
-av_abund_wild <- c(1.73e6, 0.88e6, 4.5e5)
-av_abund_total <- c(1.73e6, 0.88e6, 1.71e6)
-meta_abund_wild <- c(1.73e6, 0.88e6, 4.5e5)
-meta_abund_total <- c(1.73e6, 0.88e6, 1.71e6)
-av_cv_wild <- c(0.977, 1.005, 0.537)
-av_cv_total <- c(0.977, 1.005, 0.573)
-meta_cv_wild <- c(0.483, 0.677, 0.424)
-meta_cv_total <- c(0.483, 0.677, 0.548)
+av_abund_wild <- c(1.73e6, 0.88e6, 4.5e5, 0)
+av_abund_total <- c(1.73e6, 0.88e6, 4.5e5, 1.71e6)
+meta_abund_wild <- c(1.73e6, 0.88e6, 4.5e5, 0)
+meta_abund_total <- c(1.73e6, 0.88e6, 4.5e5, 1.71e6)
+av_cv_wild <- c(0.977, 1.005, 0.537, 0)
+av_cv_total <- c(0.977, 1.005, 0.537, 0.569)
+meta_cv_wild <- c(0.483, 0.677, 0.424, 0)
+meta_cv_total <- c(0.483, 0.677, 0.424, 0.517)
 
 av_cv_wild_log <- sqrt(log(1+(av_cv_wild*av_abund_wild)^2/(av_abund_wild^2)))
 av_cv_total_log <- sqrt(log(1+(av_cv_total*av_abund_total)^2/(av_abund_total^2)))
@@ -27,8 +27,9 @@ av_abund_total_log <- log(av_abund_total)-av_cv_wild_log^2/2
 meta_abund_wild_log <- log(meta_abund_wild)-meta_cv_wild_log^2/2
 meta_abund_total_log <- log(meta_abund_total)-meta_cv_total_log^2/2
 
+
 #name the periods
-names(meta_abund_wild) <- names(meta_abund_total) <- names(meta_cv_wild) <- names(meta_cv_total) <- names(av_cv_wild) <- names(av_cv_total) <- Period <- c("1913-1923","1933-1947","2010-2017")
+names(meta_abund_wild) <- names(meta_abund_total) <- names(meta_cv_wild) <- names(meta_cv_total) <- names(av_cv_wild) <- names(av_cv_total) <- Period <- c("1","2","3", "4")
 
 #set closure target, Period, and #iterations
 fishery_target <- 1.05e6
@@ -38,24 +39,24 @@ Nboots <- 10000
 #create 4 blank matrices that each have columns for Nboots & Nperiods
 Ntotal <- closure <- matrix(NA, Nboots, Nperiods, 
                                  dimnames = list("Boots" = 1:Nboots, 
-                                 "Period" = c("1913-1923","1933-1947","2010-2017")))
+                                 "Period" = c("1","2","3", "4")))
 
 av_closure_wild <- av_Nwild <-  matrix(NA, Nboots, Nperiods, 
                                           dimnames = list("Boots" = 1:Nboots, 
-                                                          "Period" = c("1913-1923","1933-1947","2010-2017")))
+                                                          "Period" = c("1","2","3", "4")))
 av_closure_tot <- av_Ntot <- matrix(NA, Nboots, Nperiods, 
                                         dimnames = list("Boots" = 1:Nboots, 
-                                                        "Period" = c("1913-1923","1933-1947","2010-2017")))
+                                                        "Period" = c("1","2","3", "4")))
 meta_closure_wild <- meta_Nwild <- matrix(NA, Nboots, Nperiods, 
                                          dimnames = list("Boots" = 1:Nboots, 
-                                                         "Period" = c("1913-1923","1933-1947","2010-2017")))
+                                                         "Period" = c("1","2","3", "4")))
 meta_closure_tot <- meta_Ntot <- matrix(NA, Nboots, Nperiods, 
                                  dimnames = list("Boots" = 1:Nboots, 
-                                                 "Period" = c("1913-1923","1933-1947","2010-2017")))
+                                                 "Period" = c("1","2","3", "4")))
 
 #create a blank array to receive the data
 Nt_pops <- array(NA, dim = c(Nboots, Nperiods), dimnames = list("Boots"=1:Nboots,
-                                                                "Period"= c("1913-1923","1933-1947","2010-2017")))
+                                                                "Period"= c("1","2","3", "4")))
 #run a for-loop for each period
 for(p in 1:Nperiods)
 {
@@ -87,31 +88,29 @@ for(p in 1:Nperiods)
 #meta CV wild 
 metaClosureWild <- as.data.frame(meta_closure_wild) %>%
   gather(period, closure) %>%
-  filter(period == "1913-1923") %>% #mannually change for each time-period
+  filter(period == "2") %>% #manually change for each time-period (1-4)
   summarize(avg = mean(closure))
 
 #average CV wild
 avClosureWild <- as.data.frame(av_closure_wild) %>%
   gather(period, closure) %>%
-  filter(period == "1913-1923") %>% #mannually change for each time-period
+  filter(period == "2") %>% #manually change for each time-period
   summarize(avg = mean(closure))
 
 #meta CV total (wild + enhancement)
 metaClosureTot <- as.data.frame(meta_closure_tot) %>%
   gather(period, closure) %>%
-  filter(period == "2010-2017") %>%
+  filter(period == "4") %>%
   summarize(avg = mean(closure))
 
 #average CV total (wild + enhancement)
 avClosureTot <- as.data.frame(av_closure_tot) %>%
   gather(period, closure) %>%
-  filter(period == "2010-2017") %>%
+  filter(period == "4") %>%
   summarize(avg = mean(closure))
 
-
-
-#create data.frames for Wild & Total components for plotting (1 & 2)
-#1. total abundance (average cv)
+#create data.frames for Total (wild+enhanced) & Wild components for plotting (1 & 2)
+#1. Total abundance (average cv)
 avTot <- as.data.frame(av_Ntot) %>%
   gather(period, abundance) %>%
   mutate(component = "averaged") 
@@ -123,9 +122,9 @@ metaTot <- as.data.frame(meta_Ntot) %>%
 ggTotal <- rbind(avTot, metaTot) %>%
   mutate(abundance = abundance/1000) %>% #reduce abundance to thousands 
   mutate(abundance = round(abundance, digits=0)) %>% #round digits to 0
-  filter(period == "2010-2017") #filter for recent period b/c only these data required for plot
+  filter(period == "4") #filter for recent period b/c only these data required for plot
 
-#2. wild abundance (average cv)
+#2. Wild abundance (average cv)
 avWild <- as.data.frame(av_Nwild) %>%
   gather(period, abundance) %>%
   mutate(component = "averaged")
@@ -136,7 +135,8 @@ metaWild <- as.data.frame(meta_Nwild) %>%
 #combine
 ggWild <- rbind(avWild, metaWild) %>%
   mutate(abundance = abundance/1000) %>% #reduce abundance to thousands 
-  mutate(abundance = round(abundance, digits=0)) #round digits to 0
+  mutate(abundance = round(abundance, digits=0)) %>% #round digits to 0
+  filter(period < "4")
 
 #plot
 seecol(pal_unikn)
@@ -147,12 +147,14 @@ ggplot(ggWild, aes(x=period, y=abundance, fill=component)) +
   scale_fill_manual(values=col) +
   theme_classic() +
   theme(legend.position="none") +
-  geom_hline(yintercept=1050, size=1, colour="red") +
   labs(x="Fishery period", y="Sockeye abundance (thousands)") +
   theme(axis.text=element_text(size=9),
         axis.title=element_text(size=12)) +
-  geom_violin(data = ggTotal, color="red", alpha=.2, aes(fill=component)) +
-  ggsave(file = paste0("Fishery_closure_combined.pdf")) 
+  geom_violin(data = ggTotal, aes(fill=component)) +
+  geom_hline(yintercept=1050, size=1, colour="red") +
+  scale_x_discrete(labels=c("1" = "1913-1923", "2" = "1933-1947",
+                            "3" = "2010-2017", "4" = "2010-2017"))
+  ggsave(file = paste0("Fishery_closure_combined_revised.pdf")) 
 
 #add boxplot to violins - "geom_boxplot(aes(fill=component), position = position_dodge(0.9), width=0.03)"
 
--- comparing two time periods.R
+++ comparing two time periods.R
@@ -9,41 +9,49 @@ jpeg("comparing time periods.jpeg",width=7,height=4,units="in",res=800)
 escape1 <- 0.88e6
 escape2 <- 0.88e6
 cv1 <- 1.005
+cv1_log <- sqrt(log(1+(cv1*escape1)^2/(escape1^2)))
+mu1_log <- log((escape1^2)/sqrt(escape1^2+(cv1*escape1)^2))
 cv2 <- 0.677
+cv2_log <- sqrt(log(1+(((cv2*escape2)^2)/(escape2^2))))
 thresh <- 1.05e6
-escape1.boot <- pmax(0,rnorm(1e6,mean=escape1,sd=cv1*escape1))
-escape2.boot <- pmax(0,rnorm(1e6,mean=escape2,sd=cv2*escape2))
+escape1.boot <- pmax(0,dlnorm(1e6,meanlog=log(escape1)-cv1_log^2/2,sdlog=cv1_log))
+escape2.boot <- pmax(0,rlnorm(1e6,meanlog=log(escape2)-cv2_log^2/2,sdlog=cv2_log))
+escape3 <- 1.3e6
+escape4 <- 1.3e6
+cv3 <- 1.005
+cv3_log <- log(1+(cv3*escape3)^2/(escape3^2))
+cv4 <- 0.677
+cv4_log <- log(1+(cv4*escape4)^2/(escape4^2))
+thresh <- 1.05e6
+escape3.boot <- pmax(0,rlnorm(1e6,meanlog=log(escape3)-cv3_log^2/2,sdlog=cv3_log))
+escape4.boot <- pmax(0,rlnorm(1e6,meanlog=log(escape4)-cv4_log^2/2,sdlog=cv4_log))
 
-brk.seq <- seq(min(escape1.boot,escape2.boot)-1,max(escape2.boot,escape1.boot)+1,length=100)
+
+brk.seq <- seq(min(escape1.boot,escape2.boot,escape3.boot,escape4.boot)-1,max(escape1.boot,escape2.boot,escape3.boot,escape4.boot)+1,length=100)
 
 layout(matrix(1:2,ncol=2))
 par(mar=c(3,2,1,0),mai=c(0.6,0.6,0.1,0.05))
-hist(escape1.boot,breaks=brk.seq,col=adjustcolor("dodgerblue",0.5),freq=FALSE,ylim=c(0,35e-7),main=paste("Mean abundance = ",escape1,sep=""),xlab="",cex.axis=txtSize,cex.lab=txtSize,cex.main=txtSize,xlim=c(0,6e6),ylab="")
+hist(escape1.boot,breaks=brk.seq,col=adjustcolor("dodgerblue",0.5),freq=FALSE,ylim=c(0,26e-7),main=paste("Mean abundance = ",escape1,sep=""),xlab="",cex.axis=txtSize,cex.lab=txtSize,cex.main=txtSize,xlim=c(0,max(brk.seq)),ylab="")
 mtext("Abundance",side=1,line=2,cex=txtSize)
 mtext("Density",side=2,line=2,cex=txtSize)
 hist(escape2.boot,breaks=brk.seq,col=adjustcolor("blue",0.5),add=TRUE,freq=FALSE)
 perc_clos1 <- round(100*sum(escape1.boot<=thresh)/length(escape1.boot),0)
 perc_clos2 <- round(100*sum(escape2.boot<=thresh)/length(escape2.boot),0)
-Corner_text(paste(perc_clos1,"% high variance closures","\n",perc_clos2,"% low variance closures",sep=""),"topright",cex=txtSize)
+perc_extinc1 <- round(100*sum(escape1.boot<=0)/length(escape1.boot),0)
+perc_extinc2 <- round(100*sum(escape2.boot<=0)/length(escape2.boot),0)
+Corner_text(paste(perc_clos1,"% high variance closures","\n",perc_clos2,"% low variance closures","\n\n",perc_extinc1,"% high variance extirpation","\n",perc_extinc2,"% low variance extirpation",sep=""),"topright",cex=txtSize)
 abline(v=thresh,lwd=3*txtSize,lty=3,col="red")
 
-escape1 <- 1.3e6
-escape2 <- 1.3e6
-cv1 <- 1.005
-cv2 <- 0.677
-thresh <- 1.05e6
-escape1.boot <- pmax(0,rnorm(1e6,mean=escape1,sd=cv1*escape1))
-escape2.boot <- pmax(0,rnorm(1e6,mean=escape2,sd=cv2*escape2))
-
-brk.seq <- seq(min(escape1.boot,escape2.boot)-1,max(escape2.boot,escape1.boot)+1,length=100)
 par(mar=c(0,2,1,3),mai=c(0.6,0.05,0.1,0.6))
-hist(escape1.boot,breaks=brk.seq,col=adjustcolor("dodgerblue",0.5),freq=FALSE,ylim=c(0,35e-7),main=paste("Mean abundance = ",escape1,sep=""),xlab="",cex.axis=txtSize,cex.lab=txtSize,cex.main=txtSize,xlim=c(0,6e6),yaxt="n")
+hist(escape3.boot,breaks=brk.seq,col=adjustcolor("dodgerblue",0.5),freq=FALSE,ylim=c(0,26e-7),main=paste("Mean abundance = ",escape3,sep=""),xlab="",cex.axis=txtSize,cex.lab=txtSize,cex.main=txtSize,xlim=c(0,max(brk.seq)),yaxt="n")
 mtext("Abundance",side=1,line=2,cex=txtSize)
 
-hist(escape2.boot,breaks=brk.seq,col=adjustcolor("blue",0.5),add=TRUE,freq=FALSE)
-perc_clos1 <- round(100*sum(escape1.boot<=thresh)/length(escape1.boot),0)
-perc_clos2 <- round(100*sum(escape2.boot<=thresh)/length(escape2.boot),0)
-Corner_text(paste(perc_clos1,"% high variance closures","\n",perc_clos2,"% low variance closures",sep=""),"topright",cex=txtSize)
-legend("right",c("Higher variance, same abundance","Lower variance, same abundance"),pch=22,pt.bg=c("dodgerblue","blue"),bty="n",title="Scenarios",cex=txtSize)
+hist(escape4.boot,breaks=brk.seq,col=adjustcolor("blue",0.5),add=TRUE,freq=FALSE)
+perc_clos3 <- round(100*sum(escape3.boot<=thresh)/length(escape3.boot),0)
+perc_clos4 <- round(100*sum(escape4.boot<=thresh)/length(escape4.boot),0)
+perc_extinc3 <- round(100*sum(escape3.boot<=0)/length(escape3.boot),0)
+perc_extinc4 <- round(100*sum(escape4.boot<=0)/length(escape4.boot),0)
+Corner_text(paste(perc_clos3,"% high variance closures","\n",perc_clos4,"% low variance closures","\n\n",perc_extinc3,"% high variance extirpation","\n",perc_extinc4,"% low variance extirpation",sep=""),"topright",cex=txtSize)
+legend("right",c("High variance","Low variance"),pch=22,pt.bg=c("dodgerblue","blue"),bty="n",title="Scenarios",cex=txtSize)
 abline(v=thresh,lwd=3*txtSize,lty=3,col="red")
 dev.off()
